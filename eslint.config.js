f2cae3a5fdf5513d62d1318447e5eecf0349a8f2
Timofey Kremko <timonkremko@gmail.com>
2025-10-15 01:12:52 +0300

    Update configs

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,9 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    env: {
+      node: true,
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
