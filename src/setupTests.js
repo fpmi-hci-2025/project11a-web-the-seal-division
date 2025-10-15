044943b53bf2bcb5d4b0bb3641253e6bba86f480
Timofey Kremko <timonkremko@gmail.com>
2025-10-15 13:13:35 +0300

    Update configs

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,80 @@
 import '@testing-library/jest-dom';
+import { vi } from 'vitest'
+
+global.URL = vi.fn(() => ({
+  href: '',
+  origin: '',
+  protocol: '',
+  host: '',
+  hostname: '',
+  port: '',
+  pathname: '',
+  search: '',
+  searchParams: new Map(),
+  hash: '',
+  toString: () => ''
+}))
+
+global.URLSearchParams = class {
+  constructor() {
+    this.params = new Map()
+  }
+
+  append(key, value) {
+    this.params.set(key, value)
+  }
+
+  get(key) {
+    return this.params.get(key)
+  }
+
+  getAll(key) {
+    return Array.from(this.params.values()).filter(v => v.key === key)
+  }
+
+  has(key) {
+    return this.params.has(key)
+  }
+
+  set(key, value) {
+    this.params.set(key, value)
+  }
+
+  delete(key) {
+    this.params.delete(key)
+  }
+
+  entries() {
+    return this.params.entries()
+  }
+
+  keys() {
+    return this.params.keys()
+  }
+
+  values() {
+    return this.params.values()
+  }
+
+  forEach(callback) {
+    this.params.forEach((value, key) => callback(value, key))
+  }
+
+  toString() {
+    return Array.from(this.params.entries())
+      .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+      .join('&')
+  }
+}
+
+global.TextEncoder = class {
+  encode(str) {
+    return new Uint8Array(Buffer.from(str))
+  }
+}
+
+global.TextDecoder = class {
+  decode(buffer) {
+    return Buffer.from(buffer).toString()
+  }
+}
--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,11 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js'
+    setupFiles: './src/setupTests.js',
+    environmentOptions: {
+      jsdom: {
+        resources: 'usable'
+      }
+    }
   }
 })
